fba66dc18fc3902c352681db7de0828af2fe3754
despicable me <github_anon@beltoforion.de>
2017-03-14 17:37:18 +0100

    missing files

---
+++ OrbitCalculator.h
@@ -0,0 +1,13 @@
+#ifndef ORBIT_CALCULATOR_H
+#define ORBIT_CALCULATOR_H
+
+#include "Vector.h"
+
+class OrbitCalculator
+{
+public:
+    
+    static Vec2D Compute(double angle, double a, double b, double theta, const Vec2D &p, int pertN, double pertAmp);
+};
+
+#endif
---
+++ Star.h
@@ -0,0 +1,32 @@
+#ifndef STAR_H
+#define STAR_H
+
+#include "Vector.h"
+
+/*
+class OrbitCalculator
+{
+    static Vec2D Compute(double angle, double a, double b, double theta, const Vec2D &center, double pertN, double pertAmp);
+};
+*/
+//------------------------------------------------------------------------
+class Star
+{
+public:
+
+  Star();
+  const Vec2D& CalcXY(int pertN, double pertAmp);
+
+  double m_theta;    // position auf der ellipse
+  double m_velTheta; // angular velocity
+  double m_angle;    // Schräglage der Ellipse
+  double m_a;        // kleine halbachse
+  double m_b;        // große halbachse
+  double m_temp;     // star temperature
+  double m_mag;      // brigtness;
+  Vec2D  m_center;   // center of the elliptical orbit
+  Vec2D  m_vel;      // Current velocity (calculated)
+  Vec2D  m_pos;      // current position in kartesion koordinates
+};
+
+#endif
